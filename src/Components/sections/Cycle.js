5961469c7434cbcf55e6ccf3d6eb8e2cdb83d0cf
shoaibbadshah <shoaibbadshah45@gmail.com>
2022-02-13 22:01:38 +0500

    cycle section updated

--- src/Components/sections/Cycle.js
+++ src/Components/sections/Cycle.js
@@ -2,25 +2,26 @@ import React from "react";
 
 function Cycle() {
   return (
-    <div className="col-12 flex justify-center bg-green-600">
-      <div className="col-6 ">
+    <div className="col-12  bg-green-600">
+      <div className="relative ">
         <img
-          src={`${process.env.PUBLIC_URL}/images/cycle.png`}
+          src={`${process.env.PUBLIC_URL}/images/sendy.jpg`}
           alt=""
-          className="img-fluid w-2/3 ml-48 pt-10 "
+          className="w-full"
         />
-      </div>
-      <div className="col-6 mt-32">
-        <h1 className="text-4xl font-bold mb-4 text-white">
-          We're always here
-          <br /> for you
-        </h1>
-        <p className="col-6 text-xl text-white">
-          When you’re on the road, we’re with you – for help, advice and
-          support, message or call us in the app. Your safety is our priority,
-          so we’ll insure you in case of accidents, too. It’s totally free and
-          applies from the moment you go online.
-        </p>
+
+        <div className="col-6 absolute top-1/3 left-44 z-10 ">
+          <h1 className="text-4xl font-bold mb-4 text-white">
+            We're always here
+            <br /> for you
+          </h1>
+          <p className="col-6 text-xl text-white ">
+            When you’re on the road, we’re with you – for help, advice and
+            support, message or call us in the app. Your safety is our priority,
+            so we’ll insure you in case of accidents, too. It’s totally free and
+            applies from the moment you go online.
+          </p>
+        </div>
       </div>
     </div>
   );
